2e9377fff8f181c8a770dd79292b1e0f5d7bc5bb
zhussupali <zhussupali@gmail.com>
2022-05-22 13:25:46 +0600

    jwt-token parsing

---
+++ pkg/handler/middleware.go
@@ -0,0 +1,35 @@
+package handler
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	authorizationHeader = "Authorization"
+	userCtx = "userId"
+)
+
+func (h *Handler) userIdentity(c *gin.Context) {
+	header := c.GetHeader(authorizationHeader)
+	if header == "" {
+		NewErrorResponse(c, http.StatusUnauthorized, "empty auth header")
+		return
+	}
+
+	headerParts := strings.Split(header, " ")
+	if len(headerParts) != 2 {
+		NewErrorResponse(c, http.StatusUnauthorized, "invalid auth header")
+	}
+
+	userId, err := h.services.ParseToken(headerParts[1])
+
+	if err != nil {
+		NewErrorResponse(c, http.StatusUnauthorized, err.Error())
+		return
+	}
+
+	c.Set(userCtx, userId)
+}
